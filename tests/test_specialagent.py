75a9a580d0866dfd7d952c84ff672972ba9746d5
Jon Craton <jncraton@gmail.com>
2026-09-16 14:40:53 -0400

    Remove interactive test

--- tests/test_specialagent.py
+++ tests/test_specialagent.py
@@ -3,17 +3,6 @@ from io import StringIO
 from specialagent.cli import main
 
 
-def test_quit(tmp_path, capsys, monkeypatch):
-    monkeypatch.setattr("sys.stdin", StringIO("/quit\n"))
-    try:
-        exit_code = main()
-    except SystemExit as e:
-        exit_code = e.code
-
-    captured = capsys.readouterr()
-    assert exit_code == 0
-
-
 def test_prompt_quit(capsys):
     try:
         main(["--prompt", "/quit"])
