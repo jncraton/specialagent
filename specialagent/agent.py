0dac4351d9880e19629f9e124737517e376824da
Jon Craton <jncraton@gmail.com>
2026-09-25 18:45:44 -0400

    Correct line break

--- specialagent/agent.py
+++ specialagent/agent.py
@@ -292,7 +292,7 @@ def get_system():
         print(f"Discovered {len(skills)} skills")
 
         system += (
-            "\n\n## Skills\n\cat matching skills before starting tasks:\n\n"
+            "\n\n## Skills\n\ncat matching skills before starting tasks:\n\n"
             + "\n".join(f"- `cat {k}`: {v['desc']}" for k, v in skills.items())
         )
 
